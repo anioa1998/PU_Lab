b9aa6cf5b75ab54635b1528661e8eaf190e0cde0
Anna <anioa1998@wp.pl>
2021-12-30 16:10:47 +0100

    Removed not used projects

--- ProgramowanieUzytkoweIP12/Startup.cs
+++ ProgramowanieUzytkoweIP12/Startup.cs
@@ -1,27 +1,15 @@
-
-using CQRS;
-using CQRS.AuthorFiles.Commands;
-using CQRS.AuthorFiles.Handlers;
-using CQRS.AuthorFiles.Queries;
-using CQRS.BookFiles.Commands;
-using CQRS.BookFiles.Handlers;
-using CQRS.BookFiles.Queries;
-using CQRS.Helpers;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Model.DTOs;
 using Model.Models;
 using Nest;
 using ProgramowanieUzytkoweIP12.Configuration;
 using RepositoryPattern;
 using RepositoryPattern.Helpers;
 using System;
-using System.Collections.Generic;
 
 namespace ProgramowanieUzytkoweIP12
 {
@@ -45,33 +33,15 @@ namespace ProgramowanieUzytkoweIP12
                     new ElasticClient(new ElasticConnection(new Uri("http://localhost:9200")))
 );
 
-            var assembly = AppDomain.CurrentDomain.Load("MediatRProject");
-            services.AddMediatR(assembly);
+ 
 
             services.AddDbContext<AppDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped<IBookRepository, BookRepository>();
             services.AddScoped<IAuthorRepository, AuthorRepository>();
-            services.AddScoped<IBooksHelper, BooksHelper>();
-            services.AddScoped<IAuthorsHelper, AuthorsHelper>();
             services.AddScoped<IElasticHelper, ElasticHelper>();
 
-            services.AddScoped<CommandBus>();
-            services.AddScoped<QueryBus>();
-
-            services.AddScoped<ICommandHandler<AddBookCommand>, AddBookCommandHandler>();
-            services.AddScoped<ICommandHandler<AddAuthorCommand>, AddAuthorCommandHandler>();
-
-            services.AddScoped<ICommandHandler<AddBookRateCommand>, AddBookRateCommandHandler>();
-            services.AddScoped<ICommandHandler<AddAuthorRateCommand>, AddAuthorRateCommandHandler>();
-
-            services.AddScoped<ICommandHandler<DeleteBookCommand>, DeleteBookCommandHandler>();
-            services.AddScoped<ICommandHandler<DeleteAuthorCommand>, DeleteAuthorCommandHandler>();
-
-            services.AddScoped<IQueryHandler<GetBooksQuery, List<GetBookDTO>>, GetBooksQueryHandler>();
-            services.AddScoped<IQueryHandler<GetAuthorsQuery, List<GetAuthorDTO>>, GetAuthorsQueryHandler>();
-            services.AddScoped<IQueryHandler<GetBookQuery, GetBookDTO>, GetBookQueryHandler>();
-
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
